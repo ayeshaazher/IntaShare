22a4e466459e11937fdb2c2c36d63ec0157161d0
Ayesha Azhar <ayeshaazher04@gmail.com>
2026-07-02 21:55:43 +0500

    Refactor Firebase initialization logic
    Simplified Firebase initialization for all platforms.

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -2,16 +2,5 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 Future initFirebase() async {
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-            apiKey: "AIzaSyCNWb4nMRCWl_eugeK93kucm1tfc_ICkuM",
-            authDomain: "instasharefinal-frtyuc.firebaseapp.com",
-            projectId: "instasharefinal-frtyuc",
-            storageBucket: "instasharefinal-frtyuc.firebasestorage.app",
-            messagingSenderId: "431861118983",
-            appId: "1:431861118983:web:fc11b5707c2eb6d1c652a2"));
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp();
 }
